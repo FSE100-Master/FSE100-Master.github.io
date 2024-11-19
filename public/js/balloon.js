af3f97a16223f9df106112a6ae97ae3b398f889e
FSE100-Master <daroad3@gmail.com>
2024-11-19 13:41:43 -0700

    update balloon game

--- public/js/balloon.js
+++ public/js/balloon.js
@@ -1,21 +1,34 @@
 let balloons = [];
+let clouds = [];
 let targetNumber;
 let score = 0;
 let timer = 120; // 2 minutes in seconds
 
 function setup() {
-  const canvas = createCanvas(windowWidth * .85, windowHeight * .85);
+  const canvas = createCanvas(windowWidth * 0.85, windowHeight * 0.85);
   canvas.class('center-canvas');
   generateTargetNumber();
   frameRate(60); // 60 frames per second
+
+  // Create initial clouds
+  for (let i = 0; i < 5; i++) {
+    clouds.push(new Cloud(random(width), random(height / 4), random(100, 200)));
+  }
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth * .85, windowHeight * .85);
+  resizeCanvas(windowWidth * 0.85, windowHeight * 0.85);
 }
 
 function draw() {
   background(135, 206, 235);
+  
+  // Display clouds
+  for (let cloud of clouds) {
+    cloud.move();
+    cloud.display();
+  }
+
   displayTimer();
   displayScore();
   displayTargetNumber();
@@ -105,6 +118,7 @@ class Balloon {
     this.number = number;
     this.speed = random(1, 3);
     this.size = 60;
+    this.color = color(random(255), random(255), random(255)); // Random color
   }
 
   move() {
@@ -112,7 +126,7 @@ class Balloon {
   }
 
   display() {
-    fill(255, 100, 150);
+    fill(this.color);
     ellipse(this.x, this.y, this.size, this.size + 10);
     fill(255);
     textSize(25);
@@ -123,5 +137,30 @@ class Balloon {
   isClicked(px, py) {
     let d = dist(px, py, this.x, this.y);
     return d < this.size / 2;
+  }
+}
+
+// Cloud class
+class Cloud {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.speed = random(0.5, 1.5); // Random speed for the cloud
+  }
+
+  move() {
+    this.x += this.speed; // Move the cloud to the right
+    if (this.x > width + this.size) {
+      this.x = -this.size; // Reset position to the left if it goes off-screen
+    }
+  }
+
+  display() {
+    fill(255); // White color for the cloud
+    noStroke();
+    ellipse(this.x, this.y, this.size, this.size / 2); // Main body of the cloud
+    ellipse(this.x - this.size / 4, this.y, this.size / 1.5, this.size / 3); // Left puff
+    ellipse(this.x + this.size / 4, this.y, this.size / 1.5, this.size / 3); // Right puff
   }
 }
